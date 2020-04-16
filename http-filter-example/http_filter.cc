e08a075d7b942a65a5d7a4055d725bf14302d606
heiden <heiden@192.168.0.102>
2020-04-16 08:56:24 +0800

    fix bugs and add yaml configuration

--- http-filter-example/http_filter.cc
+++ http-filter-example/http_filter.cc
@@ -27,11 +27,25 @@ const std::string HttpSampleDecoderFilter::headerValue() const {
 }
 
 FilterHeadersStatus HttpSampleDecoderFilter::decodeHeaders(RequestHeaderMap& headers, bool) {
+  // 添加一个请求头
+  headers.addCopy(headerKey(), headerValue());
+  // 设置响应体
+  auto body_text = fmt::format("{}:{}", headerKey().get(), headerValue().c_str());
+  // 添加一个响应头
+  auto modify_headers = [this](HeaderMap& headers) -> void {
+    headers.addCopy(headerKey(), headerValue());
+  };
+  decoder_callbacks_->sendLocalReply(Http::Code::OK, body_text, modify_headers, absl::nullopt, "force return");
+  // 本地响应已经发送，必须停止迭代，否则执行到Router会出现断言失败
+  return FilterHeadersStatus::StopIteration;
+}
+
+/* FilterHeadersStatus HttpSampleDecoderFilter::decodeHeaders(RequestHeaderMap& headers, bool) {
   // add a header
   headers.addCopy(headerKey(), headerValue());
 
   return FilterHeadersStatus::Continue;
-}
+} */
 
 FilterDataStatus HttpSampleDecoderFilter::decodeData(Buffer::Instance&, bool) {
   return FilterDataStatus::Continue;
